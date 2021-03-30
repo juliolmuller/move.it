a0a755559ed86b1cd3b2ce8a1085df67149f74c5
Julio Muller <juliodolago@gmail.com>
2021-03-29 23:32:42 -0300

    Added button when count-down is over

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -8,7 +8,8 @@ const POMODORO_CYCLE_SEC = SECONDS_PER_MINUTE * POMODORO_CYCLE_MIN
 let countDownTimeout: NodeJS.Timeout
 
 function CountDown() {
-  const [isActive, setActive] = useState(false)
+  const [isOver, setIsOver] = useState(false)
+  const [isActive, setIsActive] = useState(false)
   const [time, setTime] = useState(POMODORO_CYCLE_SEC)
 
   const minutes = Math.floor(time / SECONDS_PER_MINUTE)
@@ -17,8 +18,15 @@ function CountDown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
   useEffect(() => {
-    if (isActive && time > 0) {
-      countDownTimeout = setTimeout(() => setTime(time - 1), 1000)
+    if (isActive) {
+      if (time > 0) {
+        countDownTimeout = setTimeout(() => {
+          setTime(time - 1)
+        }, 1000)
+      } else if (time === 0) {
+        setIsActive(true)
+        setIsOver(true)
+      }
     }
   }, [isActive, time])
 
@@ -36,21 +44,27 @@ function CountDown() {
         </div>
       </div>
 
-      {isActive ? (
+      {isOver ? ( // eslint-disable-line no-nested-ternary
+        <button
+          type="button"
+          className={styles.button}
+          disabled
+        >Ciclo Encerrado</button>
+      ) : isActive ? (
         <button
           type="button"
           className={`${styles.button} ${styles.cycleActive}`}
           onClick={() => {
             clearTimeout(countDownTimeout)
             setTime(POMODORO_CYCLE_SEC)
-            setActive(false)
+            setIsActive(false)
           }}
         >Abandonar Ciclo</button>
       ) : (
         <button
           type="button"
           className={styles.button}
-          onClick={() => setActive(true)}
+          onClick={() => setIsActive(true)}
         >Iniciar Novo Ciclo</button>
       )}
     </>
